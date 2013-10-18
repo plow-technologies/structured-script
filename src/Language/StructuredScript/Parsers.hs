9f49c5a2b99791bfc5b737afd43a9b91f01d0a4c
Scott Murphy <scottmurphy09@gmail.com>
2013-10-18 18:31:50 -0500

    full commit for working later

--- src/Language/StructuredScript/Parsers.hs
+++ src/Language/StructuredScript/Parsers.hs
@@ -10,24 +10,11 @@ import Text.Parsec.Token
 
 {-| SST Grammer 
 
-expr  ::= var | const | ( expr ) | unop expr | expr duop expr
-var   ::= letter { letter | digit }*
-const ::= true | false
-unop  ::= - | ** | NOT
-duop  ::= & | =
 
-def = emptyDef { commentStart = "/*",
-                 commentEnd = "*/",
-                 identStart = letter,
-                 identLetter = alphaNum,
-                 nestedComments = False,
-                 caseSensitive = True,
-                 opStart = "*-<>/=&NAXOM:;",
-                 opLetter= "*-<>/=&DRT;",
-                 reservedOpNames = ["**",":=",,"NOT","*","/","MOD","+","-",">=","<=","<",">","=","<>","&","AND","OR","XOR",";"],
-                 reservedNames = ["VAR","END_VAR","FALSE","TRUE","CONST","END_CONST","IF","THEN",
-                                  "BOOL","SINT","INT","DINT","REAL","STRING","CASE","OF","FOR","DO"
-                                  ,"WHILE","REPEAT","UNTIL"]
+Useage examples: 
+
+x := true;
+
 |-}
 
 
@@ -36,7 +23,7 @@ data Expr = Var String | Con Const | Uno Unop Expr | Duo Duop Expr Expr
      deriving Show
 
 
-data Const = ConstBool Bool
+data Const = ConstBool Bool | ConstInteger Integer
            deriving (Show)
                     
 
@@ -64,52 +51,102 @@ def = emptyDef{ commentStart = "/*"
                                  "if", "then", "else", "fi",
                                  "while", "do", "od"] }
 
-TokenParser { parens     = m_parens
-            , identifier = m_identifier
-            , reservedOp = m_reservedOp
-            , reserved   = m_reserved
-            , semiSep1   = m_semiSep1
-            , whiteSpace = m_whiteSpace } = makeTokenParser def
+
+-- | Create specific parsers from the generic parser creator in Parsec
+sst_lexer :: GenTokenParser String u Identity
+sst_lexer = makeTokenParser def 
+
+sst_parens ::ParsecT String u Identity a -> ParsecT String u Identity a
+sst_parens      = parens sst_lexer   
+
+sst_identifier :: ParsecT String u Identity String
+sst_identifier  = identifier   sst_lexer 
+
+sst_reservedOp :: String -> ParsecT String u Identity ()
+sst_reservedOp  =  reservedOp   sst_lexer 
+
+sst_reserved :: String -> ParsecT String u Identity ()
+sst_reserved = reserved     sst_lexer 
+
+
+sst_semiSep1 :: ParsecT String u Identity a -> ParsecT String u Identity [a]
+sst_semiSep1    =       semiSep1     sst_lexer 
+
+sst_natural ::  ParsecT String u Identity Integer
+sst_natural     =       natural      sst_lexer 
+
+sst_integer ::  ParsecT String u Identity Integer
+sst_integer     =       integer      sst_lexer 
+
+sst_decimal ::  ParsecT String u Identity Integer
+sst_decimal     =       decimal      sst_lexer 
+
+sst_octal ::  ParsecT String u Identity Integer
+sst_octal       =       octal        sst_lexer 
+
+sst_hexadecimal ::  ParsecT String u Identity Integer
+sst_hexadecimal =       hexadecimal  sst_lexer 
+
+sst_whiteSpace :: ParsecT String u Identity ()
+sst_whiteSpace  =  whiteSpace   sst_lexer   
 
 exprparser :: Parser Expr
 exprparser = buildExpressionParser table term <?> "expression"
 
-table = [ [Prefix (m_reservedOp "~" >> return (Uno Not))]
-        , [Infix (m_reservedOp "&" >> return (Duo And)) AssocLeft]
-        , [Infix (m_reservedOp "=" >> return (Duo Iff)) AssocLeft]
+table :: [[Operator String u Identity Expr]]
+table = [ [Prefix (sst_reservedOp "~" >> return (Uno Not))]
+        , [Infix (sst_reservedOp "&" >> return (Duo And)) AssocLeft]
+        , [Infix (sst_reservedOp "=" >> return (Duo Iff)) AssocLeft]
         ]
-        
-term = m_parens exprparser
-       <|> fmap Var m_identifier
-       <|> (m_reserved "true" >> return (Con (ConstBool True)))
-       <|> (m_reserved "false" >> return (Con (ConstBool False)))
+
+term :: ParsecT String () Identity Expr        
+term = sst_parens exprparser
+       <|> fmap Var sst_identifier
+       <|> boolTParser
+       <|> boolFParser
+       <|> intParser
+
+
+boolTParser :: ParsecT String t Identity Expr
+boolTParser = (sst_reserved "true" >> return (Con (ConstBool True)))
+boolFParser :: ParsecT String t Identity Expr
+boolFParser = (sst_reserved "false" >> return (Con (ConstBool False)))
+
+
+-- | Parses any integer type constant to an Integer
+intParser :: ParsecT String u Identity Expr
+intParser = (sst_natural >>= (\x -> return (Con (ConstInteger x)))) 
+            <|> (sst_integer >>= (\x -> return (Con (ConstInteger x)))) 
+            <|> (sst_decimal >>= (\x -> return (Con (ConstInteger x))))                        
+            <|> (sst_hexadecimal >>= (\x -> return (Con (ConstInteger x))))
+            <|> (sst_octal >>= (\x -> return (Con (ConstInteger x))))
 
 
 mainparser :: Parser Stmt
-mainparser = m_whiteSpace >> stmtparser <* eof
+mainparser = sst_whiteSpace >> stmtparser <* eof
     where
       stmtparser :: Parser Stmt
-      stmtparser = fmap Seq (m_semiSep1 stmt1)
-      stmt1 = (m_reserved "nop" >> return Nop)
-              <|> do { v <- m_identifier
-                     ; m_reservedOp ":="
+      stmtparser = fmap Seq (sst_semiSep1 stmt1)
+      stmt1 = (sst_reserved "nop" >> return Nop)
+              <|> do { v <- sst_identifier
+                     ; sst_reservedOp ":="
                      ; e <- exprparser
                      ; return (v := e)
                      }
-              <|> do { m_reserved "if"
+              <|> do { sst_reserved "if"
                      ; b <- exprparser
-                     ; m_reserved "then"
+                     ; sst_reserved "then"
                      ; p <- stmtparser
-                     ; m_reserved "else"
+                     ; sst_reserved "else"
                      ; q <- stmtparser
-                     ; m_reserved "fi"
+                     ; sst_reserved "fi"
                      ; return (If b p q)
                      }
-              <|> do { m_reserved "while"
+              <|> do { sst_reserved "while"
                      ; b <- exprparser
-                     ; m_reserved "do"
+                     ; sst_reserved "do"
                      ; p <- stmtparser
-                     ; m_reserved "od"
+                     ; sst_reserved "od"
                      ; return (While b p)
                      }
 
